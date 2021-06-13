3ec817b496b01998a3527a6dceb05948204a9c3a
tikap <tikapradhan095@gmail.com>
2021-06-13 18:08:52 -0400

    Functional contact page

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 Killing two birds with one stone by combining my exploration of front-end development and technical writing.
 
 To do:
-- Add a functional Contact Me page.
+- Edit Contact Me page
+    - name and email on same row
+    - enlarge text box
 - Add About Me page content
---
+++ scripts/contact.js
@@ -0,0 +1,28 @@
+
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e){
+    
+    e.preventDefault();
+
+    let name = document.getElementById('Name_Input').value;
+    let email = document.getElementById('Email_Input').value;
+    let message = document.getElementById('Message_Input').value;
+
+    sendEmail(name, email, message);
+
+    document.querySelector(".contact-form").reset();
+}
+
+// Send the email via gmail smtp
+function sendEmail(name, email, message){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "tikapradhan095@gmail.com",
+        Password : "naevemcqafvhrqrl",
+        To : 'tikapradhan2003@yahoo.com',
+        From : email,
+        Subject : name + ' sent you a message.',
+        Body : 'Name: ' + name + ' Email: ' + email + ' Message: ' + message,
+    }).then( message => alert('Mail has been sent succesfully.'));
+}
